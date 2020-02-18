7bea7c018dfbfab47d9984805b6ceee366a1310b
Ilya Lagutovsky <ilyalatugovskii@gmail.com>
2020-02-18 13:11:52 +0300

    Initial Commit

---
+++ testBalinaSoft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testBalinaSoft
+//
+//  Created by Ilya Lagutovsky on 2/18/20.
+//  Copyright © 2020 Ilya Lagutovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
